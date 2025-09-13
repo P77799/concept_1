9d717bd6e82ac7cf0e6a94965f1c90ad27d0121e
P77799 <mrpratikneplai@gmail.com>
2025-09-13 18:41:28 +0545

    ooopsconcept

--- pro1.py
+++ pro1.py
@@ -1,13 +1,21 @@
+# def tri():
+#     line=int(input("Enter the num="))
+#     for i in range(1,line+1):
+#         for j in range(i):
+#             print("*",end="")
+#         print()
+
+#     return line
+
+# triangle=tri() 
+# print(triangle)
+
 def tri():
-    line=int(input("Enter the num="))
-    for i in range(1,line+1):
+    line=int(input("Enter how many num of line do u want?"))
+    for i in range(5,0,-1):
         for j in range(i):
-            print("*",end="")
+            print("*" ,end=" ")
         print()
-
-    return line
-
-triangle=tri() 
-print(triangle)
+tri()
 
         
